ad0d97ed454ae513c6baf6a9deef0bf3395d96ff
Gianni Gambetti <gianni@gambetti.ca>
2020-02-02 17:10:37 +0100

    add more error handling

--- main.py
+++ main.py
@@ -128,6 +128,8 @@ def get_current():
   subdomain = request.args.get('subdomain')
   ip4 = request.args.get('ip4', None)
   ip6 = request.args.get('ip6', None)
+  if not domain or not subdomain or (not ip4 and not ip6):
+    abort(400)
   return str(current_registration(domain, subdomain, ip4, ip6))
 
 
@@ -137,6 +139,8 @@ def force_update():
   subdomain = request.args.get('subdomain')
   ip4 = request.args.get('ip4', None)
   ip6 = request.args.get('ip6', None)
+  if not domain or not subdomain or (not ip4 and not ip6):
+    abort(400)
   return update_registration(domain, subdomain, ip4=ip4, ip6=ip6)
 
 
